bddd7ecc12487fb4653b58769a61361d0b0ffa3a
Signe Hoel <signe.hoel@gmail.com>
2021-01-16 16:55:56 -0400

    Cleaned up background

--- background.js
+++ background.js
@@ -3,24 +3,3 @@ chrome.runtime.onInstalled.addListener(function() {
       console.log('The color is green.');
     });
 });
-/*
-//import newton from '/newtonmath-1.js';
-import axios from "axios";
-const api = "https://newton.now.sh/api/v2/";
-
-const getResponse = async (operation, expression) => {
-    loading.style.display = "block";
-    try {
-        const response = await axios.get(`${api}/${operation}/${expression}`);
-    }
-    catch (error) {
-
-    }
-}
-
-const handleSubmit = async e => {
-    e.preventDefault();
-    getResponse("derive", "x^2");
-    console.log("done");
-}
-*/
