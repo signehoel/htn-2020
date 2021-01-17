c626457963fb8131d2052e260a0fa197bc8e15da
Signe Hoel <signe.hoel@gmail.com>
2021-01-17 02:07:39 -0400

    Drop down functionality

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
-var operation = "derive";
 var expression = "(x^2 + 2x)";
 var newExpression = expression.replace("+", "%2B");
+var operation;
 
-const api_url = 'https://newton.now.sh/api/v2/'+operation+'/'+newExpression;
+console.log(operation);
 
-async function getapi(url) { 
+var api_url = "";
+
+async function getapi() { 
     
+    operation = document.getElementById("operations").value;
+    api_url = 'https://newton.now.sh/api/v2/'+operation+'/'+newExpression;
     // Storing response 
-    const response = await fetch(url); 
+    const response = await fetch(api_url); 
     
     // Storing data in form of JSON 
     var data = await response.json(); 
@@ -15,5 +19,7 @@ async function getapi(url) {
     var result = data.result;
     console.log(result);
     resultSpace.innerHTML = result;
-} 
-getapi(api_url); 
+}
+
+var button = document.getElementById("submit");
+button.onclick = getapi;
